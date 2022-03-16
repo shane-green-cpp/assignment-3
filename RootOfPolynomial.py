d6a31767f1b4ffea39f5721dc2d02a7c0bdf0b3c
shane green <shanegreen@cpp.edu>
2022-03-16 01:50:42 -0700

    secant and hybrid working

--- RootOfPolynomial.py
+++ RootOfPolynomial.py
@@ -61,8 +61,46 @@ def newtonMethod(func, x, maxIter, eps, delta):
     print("Max iterations reached without convergence...")
     return x
 
+def secantMethod(func, a, b, maxIter, eps):
+    f = np.poly1d(func)
+    fa = f(a)
+    fb = f(b)
+
+    if abs(fa) > abs(fb):
+        a, b = b, a
+        fa, fb = fb, fa
+    
+    for it in range(1, maxIter):
+        if abs(fa) > abs(fb):
+            a, b = b, a
+            fa, fb = fb, fa
+        
+        d = (b - a) / (fb - fa)
+        b = a
+        fb = fa
+        d = d * fa
+
+        if abs(d) < eps:
+            print("Algorithm has converged after " + str(it) + " iterations!")
+            return a
+        
+        a = a - d
+        fa = f(a)
+        
+    print("Max iterations reached!")
+    return a
+
+def hybrid(func, a, b, maxIter, eps, delta):
+    print("5 iters of bisection, then newtons")
+    x = bisection(func, a, b, 5, eps)
+    r = newtonMethod(func, x, maxIter - 5, eps, delta)
+    return r
 
 print("bisection")
 print(bisection(numbers, 0, 1, iterations, epsilon))
 print("newtons")
 print(newtonMethod(numbers, 1, iterations, epsilon, delta))
+print("secant")
+print(secantMethod(numbers, 0, 1, iterations, epsilon))
+print("hybrid")
+print(hybrid(numbers, 0, 1, iterations, epsilon, delta))
